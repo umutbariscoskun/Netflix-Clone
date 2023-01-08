41c0382cd23b206ea4453a226fdce62a18e28543
umutbariscoskun <umutcoskun2017@gmail.com>
2023-01-08 18:27:29 +0300

    Hiding api keys

---
+++ Netflix Clone/Managers/.gitignore.h
@@ -0,0 +1,12 @@
+//
+//  .gitignore.h
+//  Netflix Clone
+//
+//  Created by Umut Barış Çoşkun on 8.01.2023.
+//
+
+#ifndef _gitignore_h
+#define _gitignore_h
+
+Keys.swift
+#endif /* _gitignore_h */
--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -7,10 +7,7 @@
 
 import Foundation
 
-struct Constants {
-    static let API_KEY = "b0155af1c4faade4fa09eb3692061f6f"
-    static let baseUrl = "https://api.themoviedb.org"
-}
+
 
 enum APIError: Error {
     case failedToGetData
