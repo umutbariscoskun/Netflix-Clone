40680994d68a0f62eb8a44aa2638d6dcc6068a7e
umutbariscoskun <umutcoskun2017@gmail.com>
2023-01-08 17:56:15 +0300

    Sending url request and parsing JSON response

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -29,6 +29,8 @@ class HomeViewController: UIViewController {
         
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+        
+        getTrendingMovies()
     }
     
     private func configureNavBar(){
@@ -51,6 +53,17 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
+    }
+    
+    private func getTrendingMovies(){
+        APICaller.shared.getTrendingMovies { results in
+            switch results {
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
 
---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,45 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Umut Barış Çoşkun on 8.01.2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "b0155af1c4faade4fa09eb3692061f6f"
+    static let baseUrl = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedToGetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie],Error> )-> Void){
+        guard let url = URL(string: "\(Constants.baseUrl)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {
+            data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do{
+                let results =  try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+              
+                
+            }
+            catch{
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+}
