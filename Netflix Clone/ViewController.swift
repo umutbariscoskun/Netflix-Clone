52d6e267c4d4c4af377a767b303bd35c1470c16a
umutbariscoskun <umutcoskun2017@gmail.com>
2023-01-08 13:20:14 +0300

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Umut Barış Çoşkun on 8.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
