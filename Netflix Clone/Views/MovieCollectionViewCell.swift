a8b35ecc3d300b59df0320043c2f9792400c90a5
umutbariscoskun <umutcoskun2017@gmail.com>
2023-01-09 21:45:48 +0300

    Creating Custom CollectionViewCell, add sdwebImage package

---
+++ Netflix Clone/Views/MovieCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  MovieCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Umut Barış Çoşkun on 9.01.2023.
+//
+
+import UIKit
+import SDWebImage
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    
+    static let identifier = "MovieCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else {return}
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
+    
+    
+    
+}
