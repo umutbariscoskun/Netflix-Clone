7bfa481845845e6ec624fe70dc6bd432642589ad
umutbariscoskun <umutcoskun2017@gmail.com>
2023-01-10 22:47:01 +0300

    create upcoming table cell

--- Netflix Clone/Controllers/UpComingViewController.swift
+++ Netflix Clone/Controllers/UpComingViewController.swift
@@ -8,11 +8,62 @@
 import UIKit
 
 class UpComingViewController: UIViewController {
+    
+    private var movies: [Movie] = [Movie]()
+    
+    
+    private let upcomingTable : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        fetchUpComing()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpComing(){
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let movies):
+                self?.movies = movies
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+               
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+extension UpComingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = movies[indexPath.row].original_name ?? movies[indexPath.row].original_title ?? "Unknown"
+        return cell
+    }
+    
     
 }
