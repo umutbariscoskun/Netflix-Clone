e344410366b2bc49f51355ae2a83dde35930fa45
umutbariscoskun <umutcoskun2017@gmail.com>
2023-01-11 09:28:19 +0300

    top search vc created

--- Netflix Clone/Controllers/SearchViewController.swift
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -8,14 +8,70 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private var movies : [Movie] = [Movie]()
+    
+    private let discoverTable : UITableView = {
+        let table = UITableView()
+        table.register(MovieTableViewCell.self, forCellReuseIdentifier: MovieTableViewCell.identifier)
+        return table
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        fetchDiscoverMovies()
+    }
+    
+    private func fetchDiscoverMovies(){
+        APICaller.shared.getDiscoverMovies { [weak self] result in
+            switch result {
+            case .success(let movies):
+                self?.movies = movies
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case.failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
     }
     
 
  
 
+}
+
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifier, for: indexPath) as? MovieTableViewCell else {return UITableViewCell()}
+        
+        let movie = movies[indexPath.row]
+        cell.configure(with: MovieViewModel(movieName: (movie.original_title ?? movie.original_name) ?? "Unknown" , posterUrl: movie.poster_path ?? ""))
+        return cell 
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
 }
--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -100,7 +100,30 @@ class APICaller {
     }
     
     func getTopRatedMovies(completion: @escaping (Result<[Movie],Error> )-> Void){
+        
         guard let url = URL(string: "\(Constants.baseUrl)/3/movie/top_rated?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)){
+            data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do{
+                let results =  try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+              
+                
+            }
+            catch{
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    func getDiscoverMovies(completion: @escaping (Result<[Movie],Error> )-> Void){
+        
+        guard let url = URL(string: "\(Constants.baseUrl)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return}
+        
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)){
             data, _, error in
             guard let data = data, error == nil else {
